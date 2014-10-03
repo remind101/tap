702f07cd5e14a963e4b2af7e72cb2b89fd648333
Ben Marini <ben@remind101.com>
2014-10-03 16:19:29 -0700

    Make Notify take an error type

--- worker.go
+++ worker.go
@@ -13,15 +13,15 @@ import (
 
 // ErrorHandler is an interface for draining errors to an exceptions aggregator.
 type ErrorHandler interface {
-	Notify(v interface{}) error
+	Notify(error) error
 }
 
 // NullErrorHandler is an ErrorHandler implementation that logs the error to Stderr.
 type NullErrorHandler struct{}
 
 // Notify implements the ErrorHandler Notify method.
-func (h *NullErrorHandler) Notify(v interface{}) error {
-	_, err2 := fmt.Fprintf(os.Stderr, "%v\n", v)
+func (h *NullErrorHandler) Notify(err error) error {
+	_, err2 := fmt.Fprintf(os.Stderr, "%v\n", err)
 	return err2
 }
 
